1cbca2c92a103fcb653a4261d15d8e4909d24bed
Wesley Aptekar-Cassels <W.Aptekar@gmail.com>
2015-07-13 10:15:01 -0700

    It works now, but is horrible

--- main.cpp
+++ main.cpp
@@ -4,12 +4,13 @@
 #include <algorithm>
 #include <sstream>
 #include <numeric>
+#include <functional>
 
 using namespace std;
 
 vector< vector< vector< int > > > primefactors;
 
-int gcd(int a, int b)
+int gcd(int a, int b) // find gcd
 {
   if (b == 0)
     return a;
@@ -22,13 +23,35 @@ int lcm(int a, int b)
   return (a*b)/gcd(a,b);
 }
 
+void printTable(function<int(int, int)> f, string filename) {
+	ofstream outFile;
+	outFile.open(filename);
+
+	outFile << filename.substr(0, filename.find(".")) << "\t";
+
+	for (int i = 0; i < primefactors.size() - 1; ++i){
+		outFile << primefactors[i][1][0] << "\t";
+	}
+
+	outFile << "\n";
+
+	for (int i = 0; i < primefactors.size() - 1; ++i){
+		outFile << primefactors[i][1][0] << "\t";
+		for(int um = 0; um < primefactors.size() - 1; ++um) {
+			outFile << f(primefactors[i][1][0], primefactors[um][1][0]) << "\t";
+		}
+		outFile << "\n";
+	}
+
+	outFile.close();
+}
+
 int main (){
 	string line;
 	ifstream infile;
 	infile.open("primefactor.res");
 	while(!infile.eof()) {
 		getline(infile, line);
-		cout << line << endl;
 		std::stringstream test(line);
 		std::string segment;
 		vector < vector < int > > seglist;
@@ -47,12 +70,6 @@ int main (){
 		primefactors.push_back(seglist);
 	}
 
-	for (int i = 0; i < primefactors.size() - 1; ++i){
-		for(int um = i + 1; um < primefactors.size() - 1; ++um) {
-			cout << primefactors[i][1][0] << " " << primefactors[um][1][0] << " LCM " << lcm(primefactors[i][1][0], primefactors[um][1][0]) << "\n";
-			cout << primefactors[i][1][0] << " " << primefactors[um][1][0] << " HCF " << gcd(primefactors[i][1][0], primefactors[um][1][0]) << "\n";
-		}  
-
-	}
-
+	printTable(lcm, "LCM.res");
+	printTable(gcd, "GCD.res");
 }
