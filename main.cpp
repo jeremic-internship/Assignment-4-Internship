f77e158a8f2382c9a14d4399f827337fba13f683
Wesley Aptekar-Cassels <W.Aptekar@gmail.com>
2015-07-13 12:08:07 -0700

    Refactored, used caching, is now sanic sped.

--- main.cpp
+++ main.cpp
@@ -5,40 +5,68 @@
 #include <sstream>
 #include <numeric>
 #include <functional>
+#include <utility>
+#include <cstring>
+#include <cstdlib>
+#include <map>
+#include <cmath>
 
 using namespace std;
 
-vector< vector< vector< int > > > primefactors;
+vector<pair<int, map <int, int>>> primefactors;
 
-int gcd(int a, int b) // find gcd
+vector<vector<int>> gcdmatrix;
+
+int gcd(int a1, int b1)
 {
-  if (b == 0)
-    return a;
-  else
-    return gcd(b, a%b);
+	int gcf = 1;
+	// std::cout << (a1 == b1) << std::endl;
+	if (gcdmatrix[a1][b1] != 0) {
+		gcf = gcdmatrix[a1][b1];
+	}
+	else if (gcdmatrix[b1][a1] != 0) {
+		gcf = gcdmatrix[b1][a1];
+	}
+	else {
+		map<int,int> a = primefactors[a1].second;
+		map<int,int> b = primefactors[b1].second;
+
+		for (auto it : a) {
+			auto c = b.find(it.first);
+			if (c != b.end())
+			{
+				gcf *= pow(it.first, min(it.second, c->second));
+			}
+		}
+	}
+
+	gcdmatrix[a1][b1] = gcf;
+	return gcf;
 }
 
 int lcm(int a, int b)
 {
-  return (a*b)/gcd(a,b);
+	int c = gcd(a,b);
+	cout << c << endl;
+	return (primefactors[a].first*primefactors[b].first)/c;
 }
 
-void printTable(function<int(int, int)> f, string filename) {
+void printTable(function<int(int,int)> f, string filename) {
 	ofstream outFile;
 	outFile.open(filename);
 
 	outFile << filename.substr(0, filename.find(".")) << "\t";
 
-	for (int i = 0; i < primefactors.size() - 1; ++i){
-		outFile << primefactors[i][1][0] << "\t";
+	for (int i = 0; i < primefactors.size(); i++){
+		outFile << primefactors[i].first << "\t";
 	}
 
 	outFile << "\n";
 
-	for (int i = 0; i < primefactors.size() - 1; ++i){
-		outFile << primefactors[i][1][0] << "\t";
-		for(int um = 0; um < primefactors.size() - 1; ++um) {
-			outFile << f(primefactors[i][1][0], primefactors[um][1][0]) << "\t";
+	for (int i = 0; i < primefactors.size(); i++){
+		outFile << primefactors[i].first << "\t";
+		for(int j = 0; j < primefactors.size(); j++) {
+			outFile << f(i, j) << "\t";
 		}
 		outFile << "\n";
 	}
@@ -50,24 +78,41 @@ int main (){
 	string line;
 	ifstream infile;
 	infile.open("primefactor.res");
-	while(!infile.eof()) {
-		getline(infile, line);
-		std::stringstream test(line);
-		std::string segment;
-		vector < vector < int > > seglist;
-		while(std::getline(test, segment, '='))
+	while(getline(infile, line)) {
+		stringstream liness(line);
+
+		string factorization;
+		getline(liness, factorization, '=');
+		stringstream factorss(factorization.substr(0, factorization.length() - 1));
+
+		string number;
+		getline(liness, number, '=');
+		number = number.substr(1);
+
+		pair<int, map<int, int>> entry;
+		entry.first = stoi(number);
+
+		string cur;
+		while (getline(factorss, cur, '*'))
 		{
-			vector < int > tempvector;
-			string newSegment = segment;
-			newSegment.erase(std::remove(newSegment.begin(),newSegment.end(),' '),newSegment.end());
-			std::string starsegment;
-			std::stringstream newSegmentStream(newSegment);
-			while(std::getline(newSegmentStream, starsegment, '*')){
-				tempvector.push_back(atoi(starsegment.c_str()));
+			// Process factors here
+			int val = stoi(cur);
+			if (entry.second.find(val) != entry.second.end()) {
+				entry.second[val]++;
 			}
-			seglist.push_back(tempvector);
+			else {
+				entry.second[val] = 1;
+			}
 		}
-		primefactors.push_back(seglist);
+		primefactors.push_back(entry);
+	}
+
+	for (int i = 0; i < primefactors.size(); i++) {
+		vector<int> vec;
+		for (int j = 0; j < primefactors.size(); j++) {
+			vec.push_back(0);
+		}
+		gcdmatrix.push_back(vec);
 	}
 
 	printTable(lcm, "LCM.res");
